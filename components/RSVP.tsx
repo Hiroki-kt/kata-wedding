2d4fa784afa41b02ceb2bfb98e647b529e1e5b6b
hiroki-kt <hiroki.katayama@dtosh.com>
2023-06-24 02:37:56 +0900

    make no foem rvsp section

--- components/RSVP.tsx
+++ components/RSVP.tsx
@@ -1,7 +1,14 @@
 const PVSP = () => {
   return (
-    <div className="w-full h-full bg-gray-250">
-      <h1>PVSP</h1>
+    <div className="w-full bg-gray-100 border-b-4">
+      <div className="w-[80%] m-auto pt-[100px] pb-[100px]">
+        {/* Title */}
+        <div className="pt-[100px]">
+          <h1 className="text-center text-4xl text-black">RVSP</h1>
+        </div>
+        {/* Contents1 */}
+        <div className="mt-[80px]"></div>
+      </div>
     </div>
   )
 }
